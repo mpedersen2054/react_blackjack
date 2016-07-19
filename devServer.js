c2b4d65ad1ac244ffb8ec591a6062675192cfea9
Matt Pedersen <mpedersen2054@gmail.com>
2016-07-19 17:31:40 -0500

    got it working on heroku

--- devServer.js
+++ devServer.js
@@ -12,17 +12,13 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/dist'));
 app.use(webpackMiddleware(compiler));
 
-// app.use(require('webpack-dev-middleware')(compiler, {
-//   noInfo: true,
-//   publicPath: config.output.publicPath
-// }));
-
-// app.use(require('webpack-hot-middleware')(compiler));
-
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// took me so long to find this error for pushing to production...
+// was originally: app.listen(app.get('port'), localhost, function(err) {...}
+// needed to remove localhost
 app.listen(app.get('port'), function(err) {
   if (err) {
     console.log(err);
