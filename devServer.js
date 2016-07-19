b1bd2ca14680ae90fd6b1068fe84be097e245033
Matt Pedersen <mpedersen2054@gmail.com>
2016-07-19 17:17:47 -0500

    try now..

--- devServer.js
+++ devServer.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
+var webpackMiddleware = require('webpack-dev-middleware');
 var config = require('./webpack.config.dev');
 
 var app = express();
@@ -8,15 +9,18 @@ var compiler = webpack(config);
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(express.static(__dirname + '/dist'));
+app.use(webpackMiddleware(compiler));
 
-app.use(require('webpack-hot-middleware')(compiler));
+// app.use(require('webpack-dev-middleware')(compiler, {
+//   noInfo: true,
+//   publicPath: config.output.publicPath
+// }));
+
+// app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(app.get('port'), 'localhost', function(err) {
